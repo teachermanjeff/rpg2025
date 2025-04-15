3e04b6375e11072ee1c6fca5e2986c678ab0fb6d
1301585 <131695490+1301585@users.noreply.github.com>
2025-04-15 14:41:05 -0700

    Created third level
    I was intending this to be the last level, but we can add more if you want

--- rpg-test/battle_scene.gd
+++ rpg-test/battle_scene.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_next_level_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://battle_scene2.tscn")
+
+
+func _on_end_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://winScreen.tscn")
